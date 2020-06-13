375b64b6859f87b4863079c5ec87dbdf4d6f9d1c
Blobfishman <lpkre@protonmail.com>
2020-06-13 14:39:46 +0200

    add ctags ignore & implement csv file management

--- sink/src/main.rs
+++ sink/src/main.rs
@@ -6,19 +6,26 @@ use chrono::prelude::*;
 use csv::Writer;
 use serialport::prelude::*;
 use std::error::Error;
-use std::io;
+use std::time::Duration;
+use std::thread;
 use std::path::Path;
 use std::string::String;
-use std::time::Duration;
 
 fn main() {
     //TODO get serial port
     //TODO get root_path from program arguments
     let mut session: MeasureSession = MeasureSession::setup(String::from(""), String::from(""));
+    let mut cycle = 0;
     loop {
         let val = session.receive_data();
         session.insert_data(val);
-        // TODO auto save file every 1 min
+        thread::sleep(Duration::from_millis(100));
+
+        cycle += 1;
+        if cycle >= 60000 {
+            session.save_data();
+            cycle = 0;
+        }
     }
 }
 
@@ -31,6 +38,8 @@ pub struct MeasureSession {
 
 impl MeasureSession {
     pub fn receive_data(&mut self) -> i32 {
+        let mut buffer: String;
+
 
         //TODO implement
         0
