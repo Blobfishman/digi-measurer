dcea53ccbc26e2c9918f6244f1058b970403bd8f
Blobfishman <kreienbrink@lp-kb.eu>
2020-07-02 22:36:53 +0200

    use program parameter & implement receive_data
    The new implementation is still not working because parsing the number from the string always fails

--- sink/src/main.rs
+++ sink/src/main.rs
@@ -5,69 +5,78 @@ extern crate serialport;
 use chrono::prelude::*;
 use csv::Writer;
 use serialport::prelude::*;
+use std::env;
 use std::error::Error;
-use std::time::Duration;
-use std::thread;
-use std::path::Path;
 use std::string::String;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    //TODO get serial port
-    //TODO get root_path from program arguments
-    let mut session: MeasureSession = MeasureSession::setup(String::from(""), String::from(""));
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("wrong program parameter!\nsink filename serial_port");
+        return;
+    }
+    let mut session: MeasureSession = MeasureSession::setup(&args[1], &args[2]);
     let mut cycle = 0;
     loop {
         let val = session.receive_data();
+        println!("val: {}", val);
         session.insert_data(val);
         thread::sleep(Duration::from_millis(100));
 
         cycle += 1;
         if cycle >= 60000 {
-            session.save_data();
+            session.save_data().ok();
             cycle = 0;
         }
     }
 }
 
 pub struct MeasureSession {
-    port: serialport::posix::TTYPort,
-    data: Vec<(DateTime<Local>, i32)>,
+    port: Box<dyn serialport::SerialPort>,
+    data: Vec<(DateTime<Local>, f32)>,
     device_name: String,
     root_path: String,
 }
 
 impl MeasureSession {
-    pub fn receive_data(&mut self) -> i32 {
-        let mut buffer: String;
-
-
-        //TODO implement
-        0
+    pub fn receive_data(&mut self) -> f32 {
+        let mut buffer = String::new();
+        // do-while
+        let _ = self.port.read_to_string(&mut buffer);
+        while buffer.parse::<f32>().is_err() {
+            eprintln!("reading from serial port failed");
+            println!("val: {}", buffer);
+            // ignore return value because serialport read_to_string returns an error even if it was successfull
+            let _ = self.port.read_to_string(&mut buffer);
+        }
+        buffer.parse::<f32>().unwrap()
     }
 
     // insert new values only if it is different to the last one
-    pub fn insert_data(&mut self, value: i32) {
+    pub fn insert_data(&mut self, value: f32) {
         if self.data.last().is_none() || self.data.last().unwrap().1 != value {
             self.data.push((Local::now(), value));
         }
     }
 
-    pub fn setup(path: String, serial_port: String) -> MeasureSession {
+    pub fn setup(path: &String, serial_port: &String) -> MeasureSession {
         let s = SerialPortSettings {
             baud_rate: 19200,
             data_bits: DataBits::Eight,
             flow_control: FlowControl::None,
             parity: Parity::None,
             stop_bits: StopBits::One,
-            timeout: Duration::from_millis(1),
+            timeout: Duration::from_millis(10),
         };
         MeasureSession {
-            port: serialport::posix::TTYPort::open(Path::new(&serial_port), &s)
+            port: serialport::open_with_settings(serial_port, &s)
                 .ok()
                 .unwrap(),
             data: Vec::new(),
             device_name: String::from(""),
-            root_path: path,
+            root_path: path.clone(),
         }
     }
 
